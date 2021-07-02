5f3352202eb74a8f7d9fc770eb6753adabdfbb27
Mateusz <mateusz.tryl@gmail.com>
2021-07-02 22:44:20 +0200

    First server test with printing to console

---
+++ clientSide/src/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String server = "192.168.0.227";
+        try
+        {
+            Socket socket = new Socket(server, 22);
+            PrintStream out = new PrintStream(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            getData(in);
+
+            in.close();
+            out.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void getData(BufferedReader in) throws IOException
+    {
+        String line = in.readLine();
+        while( line != null )
+        {
+            System.out.println( line );
+            line = in.readLine();
+        }
+    }
+}
---
+++ serverSide/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        PrintWriter out = null;
+        ServerSocket serverSocket = null;
+        while(true) {
+            try {
+                serverSocket = new ServerSocket(22);
+                socket = serverSocket.accept();
+                out = new PrintWriter(socket.getOutputStream());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String result;
+            String command = "python bme280.py";
+            Process p = Runtime.getRuntime().exec(command);
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((result = br.readLine()) != null) {
+                out.println(result);
+            }
+
+            out.close();
+            socket.close();
+            serverSocket.close();
+            System.out.println("Connection closed");
+        }
+        //listen for request on port
+        //get the information from sensor
+        //send information to client
+    }
+}
